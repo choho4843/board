b2f483733bd73ea38a25a3ab95c00a06eabcd8f5
choho4843 <choho4843@gmail.com>
2022-11-23 17:30:57 +0900

    img file add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import WriteBoard from './component/WriteBoard';
+import DetailBoard from './component/DetailBoard';
 
 function App() {
   return (
+    <div className='/App'>
     <WriteBoard/>
+    <DetailBoard/>
+    </div>
   );
 }
 
---
+++ src/component/DetailBoard.js
@@ -0,0 +1,75 @@
+import { Component } from "react";
+import "./WriteBoard.css"
+import axios from "axios"
+
+class DetailBoard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            writer:'',
+            subject:'',
+            content:'',
+            imageurl:''
+        }
+    }
+    componentDidMount = () => {
+        axios.get('http://localhost:8080/boarddetail/1')
+            .then((response) => {
+                const board = response.data;
+                this.setState({ writer: board.writer, subject: board.subject, content: board.content, imageUrl: 'http://localhost:8080/img/' + board.filename });
+            })
+            .catch((error) => {
+
+            })
+        }
+    render() {
+        return (
+            <>
+                <section>
+                    <h2>게시판 글 등록</h2>
+                    <form>
+                        <table>
+                            <tr>
+                                <td className="td_left">
+                                    <label for='writer'>글쓴이</label>
+                                </td>
+                                <td className="td_right">
+                                    <input type='text' name='writer' id='writer' value={this.state.writer}  />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="td_left">
+                                    <label for='subject'>제목</label>
+                                </td>
+                                <td className="td_right">
+                                    <input type='title' name='subject' id='subject' value={this.state.subject}  />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="td_left">
+                                    <label for='content'>내용</label>
+                                </td>
+                                <td className="td_right">
+                                    <textarea type='text' name='content' id='content' cols='40' rows='15' value={this.state.content}  />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="td_left">
+                                    <label for='file'>이미지파일</label>
+                                </td>
+                                <td className="td_right">
+                                    <img src={this.state.imageUrl} alt='' width={"200px"} height={"250px"}/>
+                                </td>
+                            </tr>
+                        </table>
+                        <section id='commandCell'>
+                            <a href="#">수정</a>&nbsp;&nbsp;
+                            <a href="#">삭제</a>
+                        </section>
+                    </form>
+                </section>
+            </>
+        )
+    }
+}
+export default DetailBoard;
