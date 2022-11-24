5558e1bc8c28bb99179115e6865209d3f6cc25f3
choho4843 <choho4843@gmail.com>
2022-11-24 13:02:32 +0900

    boardlist

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import WriteBoard from './component/WriteBoard';
 import DetailBoard from './component/DetailBoard';
+import ListBoard from './component/ListBoard';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className='/App'>
-    <WriteBoard/>
-    <DetailBoard/>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<ListBoard/>}/>
+      <Route exact path='/write' element={<WriteBoard/>}/>
+     <Route exact path='/detail' element={<DetailBoard/>}/>
+  </Routes>
+
   );
 }
 
---
+++ src/component/ListBoard.js
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './ListBoard.css';
+import {Link} from 'react-router-dom'
+
+function ListBoard() {
+const [boards, setBoards] = useState([]);
+
+    useEffect(() =>{
+        axios.get('http://localhost:8080/listboard').
+        then((response)=>{
+            setBoards(response.data);
+        })
+        .catch((error)=> {
+            console.log(error);
+        })
+    },[])
+    return(
+        <>
+        <h2> 글 목록 <Link to={'/write'}>게시판 글쓰기</Link></h2>
+        <section>
+            <table>
+                <tbody>
+                    <tr id='tr_top'>
+                        <th>번호</th>
+                        <th>작성자</th>
+                        <th>제목</th>
+                    </tr>
+                    {boards.map((board)=>(
+                            <tr key={board.id}>
+                                <td>{board.id}</td>
+                                <td>{board.writer}</td>
+                                <td>{board.subject}</td>
+                            </tr>
+                       ) )}
+                </tbody>
+            </table>
+        </section>
+        </>
+    )
+}
+
+export default ListBoard;
